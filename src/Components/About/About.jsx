baebed32d1586eeba6d3c1096071830250a3dbd0
ace anand <ashwinianad920@gmail.com>
2024-04-07 12:59:42 +0530

    del

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
   return (
     <div className="about">
       <div className="about-title">
-        <h1>About sMe</h1>
+        <h1>About Me</h1>
         <img src={theme_pattern} alt="" />
       </div>
       <div className="about-sections">
